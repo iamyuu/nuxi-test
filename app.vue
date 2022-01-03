dabeb9423fd9a1143eda47dd6c485c66c18eb24b
iamyuu <akuyusuf27@gmail.com>
2022-01-03 20:38:27 +0700

    feat: integration windicss

--- app.vue
+++ app.vue
@@ -1,11 +1,3 @@
-<script setup>
-useMeta({
-	bodyAttrs: {
-		class: "bg-gray-900 text-gray-50",
-	},
-})
-</script>
-
 <template>
 	<div class="bg-gray-700 m-4 p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,10 @@
 import { defineNuxtConfig } from "nuxt3"
 
 export default defineNuxtConfig({
-	buildModules: ["@unocss/nuxt"],
+	buildModules: ["nuxt-windicss"],
 	meta: {
 		bodyAttrs: {
-			// FIXME: UnoCSS not scan file nuxt config
 			class: "bg-gray-900 text-gray-50",
 		},
-	},
-	unocss: {
-		uno: true,
-		preflight: true,
 	},
 })
