6759f1bbfe25e74c07ed675bfe252f30dd642110
iamyuu <akuyusuf27@gmail.com>
2022-01-03 20:54:22 +0700

    feat: integration tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
 import { defineNuxtConfig } from "nuxt3"
 
 export default defineNuxtConfig({
-	buildModules: ["nuxt-windicss"],
 	meta: {
 		bodyAttrs: {
 			class: "bg-gray-900 text-gray-50",
+		},
+	},
+
+	css: ["~/styles/tailwind.css"],
+
+	build: {
+		postcss: {
+			postcssOptions: require("./postcss.config.js"),
 		},
 	},
 })
